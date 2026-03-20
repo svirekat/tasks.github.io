92f58b81b7ce84910d169db458bf4474eb492914
svirekat <eketerinasv@gmail.com>
2026-03-20 23:01:04 +0300

    Update dihotomia.py

--- optimization_methods/dihotomia.py
+++ optimization_methods/dihotomia.py
@@ -1,7 +1,6 @@
 epsilon = 0.5
-delta = 0.2
 l = 2*epsilon
-
+delta = 0.2
 def f(x):
     return x*x - 4*x + 6
 
@@ -26,7 +25,7 @@ while (True):
         a.append(a[k])
         ## b_k = z
         b.append(z)
-        print(f'f(y_{k}) = {fy:.4f} <= f(z_{k}) = {fz:.4f}   ->  b[{k+1}] = z_{k} = {z:.4f}')
+        print(f'f(y_{k}) = {fy:.4f} < f(z_{k}) = {fz:.4f}   ->  b[{k+1}] = z_{k} = {z:.4f}')
     else:
         ## b_k не изменится
         b.append(b[k])
@@ -36,9 +35,9 @@ while (True):
 
     L = b[k+1] - a[k+1]
     if L <= l:
-        print(f"L_{2*(k+1)} = {L:.4f} <= 1   ->   процесс поиска завершён (за {k+1} итераций)!\n")
+        print(f"L_{2*(k+1)} = {L:.4f} < 1   ->   процесс поиска завершён (за {k+1} итераций)!\n")
         print(f'x принадлежит отрезку L_{2*(k+1)} = [{a[k+1]:.4f}; {b[k+1]:.4f}]')
-        print(f'приближённое решение: x* ~= {(a[k+1] + b[k+1])/2 :.5f}\n')
+        print(f'приближённое решение: x* ~= {(a[k+1] + b[k+1])/2 :.5f} +- {L/2:.4f}\n')
         break
     else:
         print(f"L_{2*(k+1)} = {L:.4f} > 1   ->   переходим к следующей итерации.\n")
